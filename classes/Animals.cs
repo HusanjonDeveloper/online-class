dfa3257e6a7bbb963406c2fc190fba7e7e0213ba
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2022-12-12 16:50:45 +0500

    vcv

--- classes/Animals.cs
+++ classes/Animals.cs
@@ -2,6 +2,8 @@ namespace online_class.classes
 {
     public class Animals
     {
-
+        public string name { get; set; }
+        public int age { get; set; }
+        public string greeting { get; set; }
     }
 }
