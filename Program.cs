8b764cc48f4687d4bb175f372a99089dc6af9a24
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2022-12-14 02:39:16 +0500

    fdsd

--- Program.cs
+++ Program.cs
@@ -41,8 +41,16 @@ class Programm
 
         System.Console.WriteLine("Let me Tell about my friends");
 
+        string[] friendsName = new string[3];
+        friendsName[0] = "Kescha";
+        friendsName[1] = "Vasya";
+        friendsName[2] = name;
+        int[] friendsAge = { 17, 15, age };
 
-
+        for (int i = 0; i < friendsName.Length; i++)
+        {
+            System.Console.WriteLine($"{friendsName[i]} is {friendsAge[i]} year old !");
+        }
         Console.ReadKey();
     }
 }
