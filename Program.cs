a4b84cccea4a55742b50fd26e2c8e7f5d7419308
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2022-12-14 02:26:01 +0500

    ghjgv

--- Program.cs
+++ Program.cs
@@ -7,6 +7,24 @@ class Programm
         Securty securty = new Securty();
         securty.checkedPassword();
 
+        System.Console.WriteLine("Enter your name");
+        string name = Console.ReadLine();
+
+        string greeting = $"Hello{name}";
+        System.Console.WriteLine("Enter your age");
+        string agesString = Console.ReadLine();
+
+        System.Console.WriteLine("Converting...");
+
+        int age = Convert.ToInt32(agesString);
+        System.Console.WriteLine($"sECCUSSFULLY CONVERTED! {age}");
+
+        int keschasAge = 3;
+
+        int ageDiference = age - keschasAge;
+
+        System.Console.WriteLine($"the difference between your and kescha's age is {ageDiference}");
+
 
         Console.ReadKey();
     }
