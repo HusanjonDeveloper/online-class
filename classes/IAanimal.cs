5f504480e0d26a4282c056df2af8a85968ad2539
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2022-12-18 13:29:31 +0500

    interfeys

---
+++ classes/IAanimal.cs
@@ -0,0 +1,13 @@
+namespace online_class.classes
+{
+    public interface IAanimal
+    {
+        void PrintAgeDifference();
+        void AnimalsMullte();
+        void ConpareAges();
+        void TellAboutFrends();
+        int CalculateageDifferenceWhitNoReturn();
+        void Greeting();
+
+    }
+}
