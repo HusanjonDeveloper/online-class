31deb8d9562f13b60800a08bbe310bd2a57209c8
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2022-12-21 22:24:43 +0500

    dd

--- Program.cs
+++ Program.cs
@@ -6,8 +6,53 @@ class Programm
 {
     static void Main(string[] args)
     {
-        System.Console.WriteLine("enter Name:");
+        Securty securty = new Securty();
+        securty.checkedPassword();
+
+        System.Console.WriteLine("Enter your Name:");
         string name = Console.ReadLine();
 
+        string greeting = $"Hello: {name}";
+        System.Console.WriteLine(greeting);
+
+        System.Console.WriteLine("Enter your age: ");
+        string ageAsString = Console.ReadLine();
+
+        System.Console.WriteLine("Converting...");
+
+        int age = Convert.ToInt32(ageAsString);
+        System.Console.WriteLine($"Successfully converted! {age}");
+
+        int keschaAge = 3;
+
+        int AgeDifference = age - keschaAge;
+
+        System.Console.WriteLine($"the difference between you and kescha's age is {AgeDifference}");
+
+        if (age > keschaAge)
+        {
+            System.Console.WriteLine("you are older!");
+        }
+        else if (age == keschaAge)
+        {
+            System.Console.WriteLine("You are equal!");
+        }
+        else
+        {
+            System.Console.WriteLine("you are Younger!");
+        }
+
+        System.Console.WriteLine("Let me Tell you about my friends.");
+
+        string[] friendsName = new string[3];
+        friendsName[0] = "Kasee";
+        friendsName[1] = "Kescha";
+        friendsName[2] = name;
+        int[] friendsAge = { 8, 3, age };
+
+        for (int itaretion = 0; itaretion < friendsName.Length; itaretion++)
+        {
+            System.Console.WriteLine($"{friendsName[itaretion]} is {friendsAge[itaretion]} year old!");
+        }
     }
 }
