45c6af9a0634ce235a74049959f6dac3a7d9f90a
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2022-12-22 01:05:01 +0500

    dfjdjf

--- Program.cs
+++ Program.cs
@@ -2,33 +2,35 @@
 using System;
 using online_class.classes;
 
-class Programm
+namespace online_class
 {
-    static void Main(string[] args)
+    class Program
     {
-        Securty securty = new Securty();
-        securty.checkedPassword();
+        static void Main(string[] args)
+        {
+            Securty securty = new Securty();
+            securty.checkedPassword();
 
-        Animals kescha = new Animals(name: "kescha", age: 3);
+            Animals kescha = new Animals(name: "kescha", age: 3);
 
-        System.Console.WriteLine("Enter your Name:");
-        string userName = Console.ReadLine();
+            System.Console.WriteLine("Enter your Name:");
+            string userName = Console.ReadLine();
 
-        kescha.Greet(userName);
+            kescha.Greet(userName);
 
-        System.Console.WriteLine("Enter your age: ");
-        string ageAsString = Console.ReadLine();
+            System.Console.WriteLine("Enter your age: ");
+            string ageAsString = Console.ReadLine();
 
-        System.Console.WriteLine("Converting...");
+            System.Console.WriteLine("Converting...");
 
-        int userAge = Convert.ToInt32(ageAsString);
-        System.Console.WriteLine($"Successfully converted! {userAge}");
+            int userAge = Convert.ToInt32(ageAsString);
+            System.Console.WriteLine($"Successfully converted! {userAge}");
 
-        kescha.CalculetAgeDifferenceWithNoReturn(userAge);
-        kescha.PrintageDifference();
-        kescha.CompareAges(userAge);
-        kescha.TellAboutFriends(userName, userAge);
-
-
+            kescha.CalculetAgeDifferenceWithNoReturn(userAge);
+            kescha.PrintageDifference();
+            kescha.CompareAges(userAge);
+            kescha.TellAboutFriends(userName, userAge);
+            Console.ReadKey();
+        }
     }
 }
