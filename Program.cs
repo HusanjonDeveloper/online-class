12af1c8a6ecc058fbb64ad57859f0e109a7781b3
Xusanjon Muhammadaliyev <muhammadaliyevhusanjon181@gmail.com>
2022-12-22 20:53:15 +0500

    exception

--- Program.cs
+++ Program.cs
@@ -8,34 +8,43 @@ namespace online_class
     {
         static void Main(string[] args)
         {
-            Securty securty = new Securty();
-            securty.checkedPassword();
+            try
+            {
+                Securty securty = new Securty();
+                securty.checkedPassword();
 
-            Animals kescha = new Bird(name: "kescha", age: 3);
-            Animals kasee = new Pet(name: "Kasee", age: 5);
+                Animals kescha = new Bird(name: "kescha", age: 3);
+                Animals kasee = new Pet(name: "Kasee", age: 5);
 
-            System.Console.WriteLine("Enter your Name:");
-            string userName = Console.ReadLine();
+                System.Console.WriteLine("Enter your Name:");
+                string userName = Console.ReadLine();
 
-            System.Console.WriteLine("kescha says");
-            kescha.Greet(userName);
-            System.Console.WriteLine(" kasee says");
-            kasee.Greet(userName);
+                System.Console.WriteLine("kescha says");
+                kescha.Greet(userName);
+                System.Console.WriteLine(" kasee says");
+                kasee.Greet(userName);
 
-            System.Console.WriteLine("Enter your age: ");
-            string ageAsString = Console.ReadLine();
-            kasee.Greet2(userName);
-            kescha.Greet2(userName);
+                System.Console.WriteLine("Enter your age: ");
+                string ageAsString = Console.ReadLine();
+                kasee.Greet2(userName);
+                kescha.Greet2(userName);
 
-            System.Console.WriteLine("Converting...");
+                System.Console.WriteLine("Converting...");
 
-            int userAge = Convert.ToInt32(ageAsString);
-            System.Console.WriteLine($"Successfully converted! {userAge}");
+                int userAge = Convert.ToInt32(ageAsString);
+                System.Console.WriteLine($"Successfully converted! {userAge}");
 
-            kescha.CalculetAgeDifferenceWithNoReturn(userAge);
-            kescha.PrintageDifference();
-            kescha.CompareAges(userAge);
-            kescha.TellAboutFriends(userName, userAge);
+                kescha.CalculetAgeDifferenceWithNoReturn(userAge);
+                kescha.PrintageDifference();
+                kescha.CompareAges(userAge);
+                kescha.TellAboutFriends(userName, userAge);
+            }
+            catch (FormatException FormatException)
+            {
+                Console.WriteLine($"Erorr was thrown. Message: {FormatException.Message}");
+            }
+            Console.WriteLine("Program ended...");
+
             Console.ReadKey();
         }
     }
